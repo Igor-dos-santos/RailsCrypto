197f205415b982a5c62c45d22dd28f883e07dfc7
Igor Dos Santos <igoralvesdossantos02@gmail.com>
2023-01-12 17:17:06 +0000

    bootstrapped devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
   resources :posts
   get 'home/about' 
   get 'home/lookup'
   post "/home/lookup" => '/home/lookup' 
-  root 'home#index' #this will make home/index the main result of the page.
+  root 'home#index' 
 end
